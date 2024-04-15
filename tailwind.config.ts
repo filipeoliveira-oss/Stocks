8fff9e47577f4c3ef00c93ee0bcb874a2323c1ae
filipeoliveira-oss <filipeoliveira084@gmail.com>
2024-04-15 19:37:05 -0300

    Init Repo Stack changed from REACT to NEXTJS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,39 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors:{
+        'primary': {
+          100: "#d2d2d2",
+          200: "#a4a4a4",
+          300: "#777777",
+          400: "#494949",
+          500: "#1c1c1c",
+          600: "#161616",
+          700: "#111111",
+          800: "#0b0b0b",
+          900: "#060606"
+        },
+        'pyellow': {
+          100: "#ffeecc",
+          200: "#fedc9a",
+          300: "#fecb67",
+          400: "#fdb935",
+          500: "#fda802",
+          600: "#ca8602",
+          700: "#986501",
+          800: "#654301",
+          900: "#332200"
+},
       },
+      spacing: {
+        '40r':'40rem',
+        '32r':'32rem',
+        '90p':'95%'
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 };
 export default config;
